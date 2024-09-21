ea81456418d2f176025fdd46107dabfb20af27f6
ArpitaPanda-1999 <technocraft1969@gmail.com>
2024-09-21 13:24:14 +0530

    function props using

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import ClassIndex from './React-Practice/Class-index.jsx';
 import ClassIndex1 from './React-Practice/Class-index1.jsx';
 import Index from './React-Practice/Fuction-index.jsx';
 import Index1 from './React-Practice/Fuction-index1.jsx';
+import FirstPropes from './React-Practice/Function-propes.jsx';
 
 function App() {
+
+  const dataItem = [
+    {name : 'Ankita', Email : 'a@a.com', Phoneno : 1234567890},
+    {name :'Amisha', Email : 'am@a.com', Phoneno :3214567890}, 
+    {name : 'Rameswar', Email : 'r@a.com', Phoneno : 5432167890}
+  ]
+
   return (
     <>
       <Index />  <br/> <br /> 
       <Index1 />  <br/> <br/>
       <ClassIndex />  <br/> <br/>
       <ClassIndex1 /> <br/> <br/>
+      {/* <FirstPropes name = "Arpita"/> */}
+      {
+        dataItem.map((item, index) => {
+          return(
+            <FirstPropes key = {index}  name = {item.name} Email = {item.Email} Phoneno = {item.Phoneno}/>
+          )
+        })
+      }
     </>
   );
 }
